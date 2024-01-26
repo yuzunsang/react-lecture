61e204122298f3c107dba948ef9bf78933f18705
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2024-01-26 20:57:29 +0900

    #5 100

--- investment-calculator/src/components/Header/Header.jsx
+++ investment-calculator/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import reactImg from "../../assets/investment-calculator-logo.png";
+import logo from "../../assets/investment-calculator-logo.png";
 
 import "./Header.css";
 
 export default function Header() {
   return (
     <div id="header">
-      <img src={reactImg} />
+      <img src={logo} />
       <h1>React Investment Calculator</h1>
     </div>
   );
--- investment-calculator/src/components/UserInput.jsx
+++ investment-calculator/src/components/UserInput.jsx
@@ -1,30 +1,67 @@
+import { useState } from "react";
+
 export default function UserInput() {
-  function handleChange(event) {
-    console.log(event.target.value);
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevValue) => {
+      return { ...prevValue, [inputIdentifier]: newValue };
+    });
   }
 
   return (
-    <div id="user-input">
-      <form className="input-group">
-        <div>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
           <label>Initial investment</label>
-          <input type="number" />
-        </div>
-        <div>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
           <label>Annual investment</label>
-          <input type="number" />
-        </div>
-      </form>
-      <form className="input-group">
-        <div>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
           <label>Expected return</label>
-          <input type="number" />
-        </div>
-        <div>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
           <label>Duration</label>
-          <input type="number" />
-        </div>
-      </form>
-    </div>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
   );
 }
