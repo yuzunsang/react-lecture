7fe1d323eb9404b5424b2b8bb67533417b954e7e
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-03-10 12:43:08 +0900

    #8 145
    setInterval로 변경
    모달창 개선 => onReset 함수
    createPortal 개념

--- refs-portals/src/components/ResultModal.jsx
+++ refs-portals/src/components/ResultModal.jsx
@@ -1,10 +1,15 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+  // remainingTime 이 0보다 큰 지 작은 지 판별
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,20 +19,22 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
+  return createPortal(
     // 모달장을 open 과 같은 속성으로 강제로 보이게 했을 때에는 backdrop을 사용할 수 없다
-    <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost ? <h2>You lost</h2> : <h2>Your score : {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stoped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
--- refs-portals/src/components/TimerChallenge.jsx
+++ refs-portals/src/components/TimerChallenge.jsx
@@ -1,46 +1,55 @@
 import { useState, useRef } from "react";
-
 import ResultModal from "./ResultModal.jsx";
-
-// let timer;
 
 export default function TimerChallenge({ children, targetTime }) {
   const timer = useRef(); // ref 는 컴포넌트가 rerender되어도 유실되지 않음.
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
   function handleStart() {
-    // 언제나 current 속성을 target으로 해야 함.
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open(); // 외부 컴포넌트, 즉 ResultModal.jsx의 open 메소드를 참조
-    }, targetTime * 1000);
-
-    setTimerStarted(true);
+    // 남은 시간 => state에 저장
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{children}</h2>
-        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive."}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive."}
         </p>
       </section>
     </>
