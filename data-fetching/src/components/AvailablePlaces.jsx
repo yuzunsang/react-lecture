65cd84ec12da441abe80f67d2917b6c721ccb289
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2024-04-17 23:47:27 +0900

    #15 233
    백엔드로부터 data fetching 해오기
    async await 문법 사용
    useEffect로 무한 루프 방지

--- data-fetching/src/components/AvailablePlaces.jsx
+++ data-fetching/src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching Places Data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
