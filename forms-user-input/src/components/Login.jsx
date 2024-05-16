2b7fc523795e1639bca914b1529ad6720ca097cd
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-05-16 22:57:32 +0900

    #17 257
    form validation
    1. onblur 이벤트리스너 방식
    2. formSubmit 시에 validation하는 방식

--- forms-user-input/src/components/Login.jsx
+++ forms-user-input/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,7 +12,16 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("Send HTTP Request...");
   }
 
   return (
@@ -20,7 +31,10 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email} />
+          <input id="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter valid email.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
